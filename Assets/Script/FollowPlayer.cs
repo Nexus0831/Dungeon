2e0627a87f57db214e809712d411161095b657ba
Nexus0831 <pride0831@gmail.com>
2018-05-13 20:02:37 +0900

    feat: 広場と回転カメラを追加

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -16,5 +16,18 @@ public class FollowPlayer : MonoBehaviour
     {
         // 自分自身の座標に、targetの座標に相対座標を足した値を設定する
         GetComponent<Transform>().position = target.position + offset;
+
+        // マウスの右クリックを押している間
+        if (Input.GetMouseButton(1))
+        {
+            // マウスの移動量
+            float mouseInputX = Input.GetAxis("Mouse X");
+            float mouseInputY = Input.GetAxis("Mouse Y");
+            // targetの位置のY軸を中心に、回転（公転）する
+            //GetComponent<Transform>().RotateAround(offset, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+            GetComponent<Transform>().RotateAround(offset, Vector3.up, mouseInputX * Time.deltaTime * 200f);
+            // カメラの垂直移動（※角度制限なし、必要が無ければコメントアウト）
+            GetComponent<Transform>().RotateAround(offset, transform.right, mouseInputY * Time.deltaTime * 200f);
+        }
     }
 }
